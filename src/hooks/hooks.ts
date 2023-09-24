378379d8f1120491c60cbf52a020c7055ec1c4ec
Vignesh Srinivasa Raghavan <66298813+vigneshonlinetutor@users.noreply.github.com>
2023-09-24 13:08:45 +0200

    Update hooks.ts

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,12 +1,17 @@
-import {After, Before} from '@cucumber/cucumber';
-import { Browser, Page, chromium } from '@playwright/test';
+import {After, AfterAll, Before, BeforeAll} from '@cucumber/cucumber';
+import { Browser, BrowserContext, Page, chromium } from '@playwright/test';
 import { pageFixture} from './pageFixture';
 
 let browser: Browser;
 let page: Page;
+let context: BrowserContext
+
+BeforeAll(async function(){
+    browser = await chromium.launch({headless:false});
+})
 
 Before(async function(){
-    browser = await chromium.launch({headless:false});
+    context = await browser.newContext();
     page = await browser.newPage();
     // @ts-ignore
     pageFixture.page = page;
@@ -14,5 +19,9 @@ Before(async function(){
 
 After(async function(){
     await page.close();
+    await context.close();
+})
+
+AfterAll(async function(){
     await browser.close();
 })
