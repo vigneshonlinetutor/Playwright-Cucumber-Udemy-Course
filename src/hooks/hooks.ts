beb49c3a670ec86c28bcc601cd8eefd1ba58ef2b
Vignesh Srinivasa Raghavan <66298813+vigneshonlinetutor@users.noreply.github.com>
2023-09-24 13:39:01 +0200

    Added Cucumber HTML Report

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,27 +1,43 @@
-import {After, AfterAll, Before, BeforeAll} from '@cucumber/cucumber';
+import { After, AfterAll, AfterStep, Before, BeforeAll, Status } from '@cucumber/cucumber';
 import { Browser, BrowserContext, Page, chromium } from '@playwright/test';
-import { pageFixture} from './pageFixture';
+import { pageFixture } from './pageFixture';
 
 let browser: Browser;
 let page: Page;
 let context: BrowserContext
 
-BeforeAll(async function(){
-    browser = await chromium.launch({headless:false});
+BeforeAll(async function () {
+    browser = await chromium.launch({ headless: false });
 })
 
-Before(async function(){
+Before(async function () {
     context = await browser.newContext();
     page = await browser.newPage();
     // @ts-ignore
     pageFixture.page = page;
 })
 
-After(async function(){
+After(async function ({ pickle, result }) {
+    // Screenshot only for failure
+    if(result?.status == Status.FAILED){
+        const image = await pageFixture.page.screenshot({path:`./test-result/screenshots/${pickle.name}.png`, type: "png"});
+        await this.attach(image, "image/png");
+    }
+
+    // Screenshot after each scenario
+    // const image = await pageFixture.page.screenshot({ path: `./test-result/screenshots/${pickle.name}.png`, type: "png" });
+    // await this.attach(image, "image/png");
+
     await page.close();
     await context.close();
 })
 
-AfterAll(async function(){
+AfterAll(async function () {
     await browser.close();
-})
+});
+
+// AfterStep(async function({pickle}){
+//     // Screenshot after each step
+//     const image = await pageFixture.page.screenshot({ path: `./test-result/screenshots/${pickle.name}.png`, type: "png" });
+//     await this.attach(image, "image/png");
+// })
